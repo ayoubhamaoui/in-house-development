740ab406b29ccc6b74d6f100c5348826f313b6a2
ayoub <ayoub10hamaoui@gmail.com>
2019-04-05 19:44:28 +0100

    fix port

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const port = process.env.PORT || 5000
 
 const db = require('./queries')
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
@@ -12,9 +17,11 @@ app.use(
   })
 )
 
-app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API' })
-  })
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 
   app.get('/users', db.getUsers)
   app.get('/users/:id', db.getUserById)
