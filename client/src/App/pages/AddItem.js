bb6555e35657979775b31030dd1913932cb3dc27
ayoub <ayoub10hamaoui@gmail.com>
2019-04-07 17:27:12 +0100

    test post with axios

--- client/src/App/pages/AddItem.js
+++ client/src/App/pages/AddItem.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import axios from  'axios'
 
 
 class AddItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      name:'',
+      email:''
+    }
+
+  }
+
+  changeHandler = (e) =>{
+    this.setState({[e.target.name]: e.target.value })
+  }
+
+  submitHandler = e =>{
+    e.preventDefault()
+    console.log(this.state)
+    axios.post('/users', this.state)
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error=>{
+      console.log(error)
+    })
+  }
+
     render() {
+      const {name,email} =this.state
       return (
       <section>
       <div className="container">
 
-      <form className="form-signin" style={{
+      <form onSubmit={this.submitHandler} className="form-signin" style={{
         "width": "100%",
         "maxWidth": "420px",
         "padding": "15px",
@@ -19,17 +46,14 @@ class AddItem extends Component {
     </div>
 
   <div className="form-label-group">
-    <input type="text" id="input" className="form-control" placeholder="Email address" required="" autoFocus=""/>
-    <label htmlFor="inputEmail">Email address</label>
+    <input type="text" className="form-control" name="name"  value={name} onChange={this.changeHandler} />
   </div>
 
   <div className="form-label-group">
-    <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required="" autoFocus=""/>
-    <label htmlFor="inputEmail">Email address</label>
+    <input type="text" className="form-control" name="email" value={email} onChange={this.changeHandler} />
   </div>
 
   <button className="btn btn-lg btn-primary btn-block" type="submit">Add</button>
-  <p className="mt-5 mb-3 text-muted text-center">© 2017-2019</p>
 </form>
       </div>
       </section>
